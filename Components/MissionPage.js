11370c4424ca54683f3d978e48b2292f398f0575
HP657 <junlee5070@gmail.com>
2024-09-04 19:59:50 +0900

    챗, 홈, 미션, 포인트 페이지 대충 생성

---
+++ Components/MissionPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const MissionPage = () => (
+  <View style={styles.container}>
+    <Text>미션 화면</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default MissionPage;
---
+++ Components/PointsPage.js
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+const PointsPage = () => {
+  return (
+    <View style={styles.container}>
+      <Text>PointsPage</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default PointsPage;
