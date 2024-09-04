eb4e6136fe5ad2dad1e694459b11117abf6b74ce
HP657 <junlee5070@gmail.com>
2024-09-04 19:59:17 +0900

    푸터 선택에 따라 화면 변경 가능케 함

--- App.js
+++ App.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomePage from "./Components/HomePage";
+import MissionPage from "./Components/MissionPage";
+import ChatPage from "./Components/ChatPage";
+import PointsPage from "./Components/PointsPage";
 import RankingPage from "./Components/RankingPage";
+import Footer from "./Components/Footer";
+
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <RankingPage />
+    <NavigationContainer>
+      <Tab.Navigator tabBar={(props) => <Footer {...props} />}>
+        <Tab.Screen
+          name="Home"
+          component={HomePage}
+          options={{ tabBarLabel: "홈", headerShown: false }}
+        />
+        <Tab.Screen
+          name="Mission"
+          component={MissionPage}
+          options={{ tabBarLabel: "미션", headerShown: false }}
+        />
+        <Tab.Screen
+          name="Chat"
+          component={ChatPage}
+          options={{ tabBarLabel: "채팅", headerShown: false }}
+        />
+        <Tab.Screen
+          name="Ranking"
+          component={RankingPage}
+          options={{ tabBarLabel: "랭킹", headerShown: false }}
+        />
+        <Tab.Screen
+          name="Points"
+          component={PointsPage}
+          options={{ tabBarLabel: "포인트 상점", headerShown: false }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
