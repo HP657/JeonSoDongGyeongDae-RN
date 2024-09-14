3bcba3adf5d16bcb228ba3bfa7d4839f2da8570a
HP657 <junlee5070@gmail.com>
2024-09-14 19:15:32 +0900

    인트로 설정

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomePage from "./Components/HomePage";
 import MissionPage from "./Components/MissionPage";
@@ -7,44 +8,55 @@ import ChatPage from "./Components/ChatPage";
 import PointsPage from "./Components/PointsPage";
 import RankingPage from "./Components/RankingPage";
 import Footer from "./Components/Footer";
+import SplashScreen from "./Components/SplashScreen";
 
+const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const MainTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      tabBar={(props) => <Footer {...props} />}
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomePage}
+        options={{ tabBarLabel: "홈" }}
+      />
+      <Tab.Screen
+        name="Mission"
+        component={MissionPage}
+        options={{ tabBarLabel: "미션" }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={ChatPage}
+        options={{ tabBarLabel: "채팅" }}
+      />
+      <Tab.Screen
+        name="Ranking"
+        component={RankingPage}
+        options={{ tabBarLabel: "랭킹" }}
+      />
+      <Tab.Screen
+        name="Points"
+        component={PointsPage}
+        options={{ tabBarLabel: "포인트 상점" }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        tabBar={(props) => <Footer {...props} />}
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomePage}
-          options={{ tabBarLabel: "홈", headerShown: false }}
-        />
-        <Tab.Screen
-          name="Mission"
-          component={MissionPage}
-          options={{ tabBarLabel: "미션", headerShown: false }}
-        />
-        <Tab.Screen
-          name="Chat"
-          component={ChatPage}
-          options={{ tabBarLabel: "채팅", headerShown: false }}
-        />
-        <Tab.Screen
-          name="Ranking"
-          component={RankingPage}
-          options={{ tabBarLabel: "랭킹", headerShown: false }}
-        />
-        <Tab.Screen
-          name="Points"
-          component={PointsPage}
-          options={{ tabBarLabel: "포인트 상점", headerShown: false }}
-        />
-      </Tab.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Main" component={MainTabNavigator} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
---
+++ Components/SplashScreen.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useRef } from "react";
+import { StyleSheet, View, Image, Animated } from "react-native";
+import splashImage from "./../assets/co2icon.png"; // Update the path if necessary
+
+export default function SplashScreen({ navigation }) {
+  const fillAnimation = useRef(new Animated.Value(0)).current;
+  const imageAnimation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(fillAnimation, {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: false,
+      }),
+      Animated.timing(imageAnimation, {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: false,
+      }),
+    ]).start(() => {
+      navigation.replace("Main");
+    });
+  }, [fillAnimation, imageAnimation, navigation]);
+
+  const fillBackgroundColor = fillAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["#00A34F", "#27b338"],
+  });
+
+  const imageScale = imageAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0.5, 1],
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          styles.fill,
+          {
+            backgroundColor: fillBackgroundColor,
+            transform: [
+              {
+                translateY: fillAnimation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [800, 0],
+                }),
+              },
+            ],
+          },
+        ]}
+      />
+      <Animated.Image
+        style={[styles.image, { transform: [{ scale: imageScale }] }]}
+        source={splashImage}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#A8D08D",
+  },
+  fill: {
+    position: "absolute",
+    bottom: 0,
+    height: "100%",
+    width: "100%",
+  },
+  image: {
+    width: 200,
+    height: 400,
+    resizeMode: "contain",
+    zIndex: 1,
+  },
+});
