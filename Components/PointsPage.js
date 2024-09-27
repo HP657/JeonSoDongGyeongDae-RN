ad91a89ff8c755ef1a1e12ca6fff77b7e1b3cc0c
HP657 <junlee5070@gmail.com>
2024-09-27 14:33:02 +0900

    대충 만듬

--- Components/PointsPage.js
+++ Components/PointsPage.js
@@ -1,9 +1,25 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView, Image } from "react-native";
 
 const PointsPage = () => {
   return (
     <View style={styles.container}>
-      <Text>PointsPage</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>포인트 상점</Text>
+      </View>
+      <ScrollView horizontal={true}>
+        <Image
+          source={require("./../assets/mission1.png")}
+          style={styles.image}
+        />
+        <Image
+          source={require("./../assets/mission2.png")}
+          style={styles.image}
+        />
+        <Image
+          source={require("./../assets/mission3.png")}
+          style={styles.image}
+        />
+      </ScrollView>
     </View>
   );
 };
@@ -11,8 +27,19 @@ const PointsPage = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "#FFFFFF",
+  },
+  headerText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    alignSelf: "center",
+    marginTop: 50,
+  },
+  image: {
+    width: 100, // Adjust width as needed
+    height: 100, // Adjust height as needed
+    marginRight: 10, // Space between images
   },
 });
 
