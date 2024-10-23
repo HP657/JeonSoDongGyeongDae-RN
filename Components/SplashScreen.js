2c15272ae7bf138e5f49f9b0a8a78a192464cd5c
HP657 <junlee5070@gmail.com>
2024-10-24 00:08:13 +0900

    토큰을 확인하여 로그인 상태를 확인하여 네비게이트 실행

--- Components/SplashScreen.js
+++ Components/SplashScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { StyleSheet, View, Animated } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage"; // AsyncStorage 임포트
 import splashImage from "./../assets/co2icon.png";
 
 export default function SplashScreen({ navigation }) {
@@ -7,24 +8,39 @@ export default function SplashScreen({ navigation }) {
   const imageAnimation = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    Animated.parallel([
-      Animated.timing(fillAnimation, {
-        toValue: 1,
-        duration: 2000,
-        useNativeDriver: false,
-      }),
-      Animated.timing(imageAnimation, {
-        toValue: 1,
-        duration: 2000,
-        useNativeDriver: false,
-      }),
-    ]).start(() => {
-      if (true) {
+    const checkTokenAndNavigate = async () => {
+      try {
+        // AsyncStorage에서 토큰 불러오기
+        const token = await AsyncStorage.getItem("accessToken");
+
+        // 애니메이션 시작
+        Animated.parallel([
+          Animated.timing(fillAnimation, {
+            toValue: 1,
+            duration: 2000,
+            useNativeDriver: false,
+          }),
+          Animated.timing(imageAnimation, {
+            toValue: 1,
+            duration: 2000,
+            useNativeDriver: false,
+          }),
+        ]).start(() => {
+          // 토큰이 있으면 Main으로, 없으면 Login으로 이동
+          if (token) {
+            navigation.replace("Main");
+          } else {
+            navigation.replace("Login");
+          }
+        });
+      } catch (error) {
+        console.log("토큰 확인 중 오류 발생:", error);
+        // 오류가 발생한 경우 Login으로 이동
         navigation.replace("Login");
-      } else {
-        navigation.replace("Main");
       }
-    });
+    };
+
+    checkTokenAndNavigate(); // 토큰 확인 함수 호출
   }, [fillAnimation, imageAnimation, navigation]);
 
   const fillBackgroundColor = fillAnimation.interpolate({
